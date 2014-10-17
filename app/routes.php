38133ed4d470de21642f0a72568492ef2a8695bb
Chinmay Patel <contactme@chinmay.ca>
2014-10-16 20:59:10 -0400

    Pushing Routes

--- app/routes.php
+++ app/routes.php
@@ -13,9 +13,39 @@
 
 Route::get('/', function()
 {
-	$response_array = array(
-		'method' => 'get', 
-	);
+	$response_array = parse_information('GET');
 	return json_encode($response_array); 
-	// return View::make('hello');
 });
+
+Route::post('/', function()
+{
+	$response_array = parse_information('GET');
+	return json_encode($response_array); 
+});
+
+Route::put('/', function()
+{
+	$response_array = parse_information('PUT');
+	return json_encode($response_array); 
+});
+
+Route::delete('/', function()
+{
+	$response_array = parse_information('DELETE');
+	return json_encode($response_array); 
+});
+
+Route::patch('/', function()
+{
+	$response_array = parse_information('PATCH');
+	return json_encode($response_array); 
+});
+
+
+function parse_information( $method ){
+	$response_array = array();
+	$response_array['method'] = $method;
+	$response_array['headeres'] = Request::header();
+	$response_array['data'] = Input::all();
+	return $response_array;
+}
